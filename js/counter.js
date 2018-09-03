7ecfbf1c7c154cd5d773fa8b824e7e2abfa7120c
@Unangst <unangst@gmail.com>
2018-09-03 10:57:31 -0400

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -1,6 +1,25 @@
-$('.counter').counterUp({
-  delay: 10,
-  time: 2000
+$('.counter').each(function() {
+  var $this = $(this),
+      countTo = $this.attr('data-count');
+  
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
+
+  {
+
+    duration: 8000,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+      //alert('finished');
+    }
+
+  });  
+  
+  
+
 });
-$('.counter').addClass('animated fadeInDownBig');
-$('h3').addClass('animated fadeIn');
